38de3becd21d0aa8eede8b164ccd7dcd1c4fbe0d
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-05-20 22:43:18 -0400

    Set up search component and updated display to grid

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,25 @@
+import posters from "./data/posters";
+import SearchBar from "./components/SearchBar";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  //return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex flex-col">
+      <SearchBar />
+      <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 items-center justify-center gap-4 px-2 py-2">
+        {posters.map((poster) => (
+          <div key={poster.id} className="shadow-md rounded-md px-2">
+            <img
+              src={poster.imageUrl}
+              alt={poster.title}
+              className="rounded-lg"
+            />
+            <h2>{poster.title}</h2>
+            <p>{poster.description}</p>
+            <p>{`$${poster.price}`}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ client/src/data/posters.js
@@ -0,0 +1,90 @@
+import posterImage from '../assets/poster1.jpg';
+
+const posters = [
+    {
+        id: 1,
+        title: "The Lurking Vampire",
+        description: "A vampire lurks in the shadows. An 80's cartoon style horror depiction of a vampire in the woods.",
+        price: 19.99,
+        imageUrl: posterImage
+    },
+    {
+        id: 2,
+        title: "The Lurking Vampire",
+        description: "A vampire lurks in the shadows. An 80's cartoon style horror depiction of a vampire in the woods.",
+        price: 19.99,
+        imageUrl: posterImage
+    },
+    {
+        id: 3,
+        title: "The Lurking Vampire",
+        description: "A vampire lurks in the shadows. An 80's cartoon style horror depiction of a vampire in the woods.",
+        price: 19.99,
+        imageUrl: posterImage
+    },
+    {
+        id: 4,
+        title: "The Lurking Vampire",
+        description: "A vampire lurks in the shadows. An 80's cartoon style horror depiction of a vampire in the woods.",
+        price: 19.99,
+        imageUrl: posterImage
+    },
+    {
+        id: 5,
+        title: "The Lurking Vampire",
+        description: "A vampire lurks in the shadows. An 80's cartoon style horror depiction of a vampire in the woods.",
+        price: 19.99,
+        imageUrl: posterImage
+    },
+    {
+        id: 6,
+        title: "The Lurking Vampire",
+        description: "A vampire lurks in the shadows. An 80's cartoon style horror depiction of a vampire in the woods.",
+        price: 19.99,
+        imageUrl: posterImage
+    },
+    {
+        id: 7,
+        title: "The Lurking Vampire",
+        description: "A vampire lurks in the shadows. An 80's cartoon style horror depiction of a vampire in the woods.",
+        price: 19.99,
+        imageUrl: posterImage
+    },
+    {
+        id: 8,
+        title: "The Lurking Vampire",
+        description: "A vampire lurks in the shadows. An 80's cartoon style horror depiction of a vampire in the woods.",
+        price: 19.99,
+        imageUrl: posterImage
+    },
+    {
+        id: 9,
+        title: "The Lurking Vampire",
+        description: "A vampire lurks in the shadows. An 80's cartoon style horror depiction of a vampire in the woods.",
+        price: 19.99,
+        imageUrl: posterImage
+    },
+    {
+        id: 10,
+        title: "The Lurking Vampire",
+        description: "A vampire lurks in the shadows. An 80's cartoon style horror depiction of a vampire in the woods.",
+        price: 19.99,
+        imageUrl: posterImage
+    },
+    {
+        id: 11,
+        title: "The Lurking Vampire",
+        description: "A vampire lurks in the shadows. An 80's cartoon style horror depiction of a vampire in the woods.",
+        price: 19.99,
+        imageUrl: posterImage
+    },
+    {
+        id: 12,
+        title: "The Lurking Vampire",
+        description: "A vampire lurks in the shadows. An 80's cartoon style horror depiction of a vampire in the woods.",
+        price: 19.99,
+        imageUrl: posterImage
+    },
+];
+
+export default posters;
