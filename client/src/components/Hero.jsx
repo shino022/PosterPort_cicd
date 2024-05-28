627618a821f0941550bdf8647117326bb1ac49d4
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-05-28 16:24:59 -0400

    Updated hero component with p tag

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,8 +1,8 @@
 const Hero = () => {
   return (
     <div className="flex flec-col items-center justify-center w-full bg-violet-900 h-[250px]">
-      <h1 className="text-6xl font-jersey italic text-orange-500">WELCOME TO POSTER PORT</h1>
-      <p></p>
+      <h1 className="text-6xl font-jersey text-orange-500">WELCOME TO POSTER PORT</h1>
+      <p>Where </p>
     </div>
   )
 }
