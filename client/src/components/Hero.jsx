c40dccd04190d450bcfdbcc66519c31fd563759d
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-05-29 09:01:28 -0400

    Updated Header margin and added cancel button to payment form

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-full bg-violet-900 max-h-[500px] py-2">
+    <div className="flex flex-col items-center justify-center w-full bg-violet-900 max-h-[500px] py-2 mb-2">
       <h1 className="text-6xl text-center font-jersey text-orange-500">WELCOME TO POSTER PORT</h1>
       <p className="hidden sm:block sm:mx-auto text-xl lg:mx-[300px] font-jersey text-orange-500 text-center">Dive into the vibrant world of Poster Port, a captivating online destination where art meets accessibility. This unique website features a diverse collection of illustrations, each carefully categorized to enhance your browsing experience. Whether you're a fan of eerie horror scenes, serene landscapes, or dynamic urban sketches, Poster Port offers a visual feast for every taste. Ideal for art enthusiasts and casual browsers alike, Poster Port invites you to explore its rich gallery, find inspiration, and perhaps discover the perfect piece to grace your space or to share with others who appreciate the beauty and diversity of illustrated art.</p>
     </div>
--- client/src/components/Payment.jsx
+++ client/src/components/Payment.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import cardValidator from "card-validator";
+import { Link } from "react-router-dom";
 
 const PaymentPage = () => {
   const [cardType, setCardType] = useState(""); // State to store the card type
@@ -54,7 +55,7 @@ const PaymentPage = () => {
     setValue("cvv", value, { shouldValidate: true });
   };
   return (
-    <div className="flex items-center justify-center min-h-screen px-1">
+    <div className="flex items-center justify-center min-h-screen min-w-[350px] px-1">
       <div className="w-[500px] mx-auto p-4 bg-white rounded-lg shadow-md">
         <h2 className="text-2xl font-bold mb-4">Payment Information</h2>
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
@@ -143,7 +144,7 @@ const PaymentPage = () => {
                 maxLength: {
                   value: 3,
                   message: "CVV must be exactly 3 digits",
-                },                
+                },
               })}
               className="block w-1/6 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 text-center"
               onChange={formatCVV}
@@ -160,13 +161,24 @@ const PaymentPage = () => {
             <p className="text-green-500">Payment successful!</p>
           )}
 
-          <button
-            type="submit"
-            disabled={isSubmitting}
-            className="w-full px-4 py-2 text-white bg-blue-500 rounded-md shadow-md hover:bg-blue-600"
-          >
-            Submit Payment
-          </button>
+          <div className="w-full flex gap-2">
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="w-full px-4 py-2 text-white bg-blue-500 rounded-md shadow-md hover:bg-blue-600"
+            >
+              Submit Payment
+            </button>
+            <Link to="/" className="w-full">
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className="w-full px-4 py-2 text-white bg-red-500 rounded-md shadow-md hover:bg-red-600"
+              >
+                Cancel Payment
+              </button>
+            </Link>
+          </div>
         </form>
       </div>
     </div>
