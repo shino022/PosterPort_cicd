d7c1f96eda11ed8f6dfc47dd4b3c9bb44587c4e6
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-06-08 15:38:02 -0400

    Added a backward link to cart on payment page

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -8,56 +8,93 @@
 
 // export default Cart;
 
-
-import React from 'react';
+import { useEffect, useState } from "react";
 import posters from "../data/posters.js";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { RxArrowTopRight } from "react-icons/rx";
 
 const Cart = ({ counts, incrementCount, decrementCount }) => {
   // Create a list of posters that are actually in the cart
-  const cartItems = posters.filter(poster => counts[poster.id] > 0);
-  console.log(cartItems);
+  const cartItems = posters.filter((poster) => counts[poster.id] > 0);
+  const [cartTotal, setCartTotal] = useState(0);
+
+  useEffect(() => {
+    let newTotal = 0;
+    cartItems.forEach((poster) => {
+      newTotal += counts[poster.id] * poster.price;
+    });
+    setCartTotal(newTotal.toFixed(2));
+  }, [counts, cartItems]);
 
   return (
     <div className="flex justify-center w-full min-h-[100vh]">
       <div className="bg-amber-500 w-[70%] min-w-[350px] px-4 pb-2 opacity-95">
-        <h2 className='text-white text-center m-2 text-3xl'>YOUR CART</h2>
-        <div>
-          <Link to='/'>
-            <button className='flex group justify-center items-center gap-1 text-xl text-amber-500 bg-indigo-600 p-2 rounded-md hover:bg-indigo-700'>HOME <RxArrowTopRight className='group-hover:rotate-45' /></button>
-          </Link>
-        </div>
+        <h2 className="text-white text-center m-2 text-3xl">YOUR CART</h2>
+        <Link to="/" style={{ display: "inline-block" }}>
+          <button className="flex group justify-center items-center gap-1 text-xl text-amber-500 bg-indigo-600 p-2 rounded-md hover:bg-indigo-700">
+            HOME <RxArrowTopRight className="group-hover:rotate-45" />
+          </button>
+        </Link>
         {cartItems.length > 0 ? (
-          <ul className='text-white grid grid-cols-1 md:grid-cols-2 gap-2 text-xl'>
-            {cartItems.map(poster => (
-              <div className='w-full bg-gray-500 p-2 mt-2 rounded-md border-2 border-indigo-700'>
-                <li key={poster.id} className='flex items-center gap-1 md:gap-16'>
-                  <div>
-                    <h3>{poster.title}</h3>
-                    <img
-                      src={poster.imageUrl}
-                      alt={poster.title}
-                      className="rounded-lg border border-blue-500 border-opacity-50 h-[300px]"
-                    />
-                  </div>
-                  <div>
-                    <p>Quantity: {counts[poster.id]}</p>
-                    <p>$$ per item: ${poster.price}</p>
-                    <div className='w-full'>
-                      <button className='bg-green-500 p-2 rounded-md w-10 hover:bg-green-700 text-xl mt-2 mr-2' onClick={() => incrementCount(poster.id)}>+</button>
-                      <button className='bg-red-500 p-2 rounded-md w-10 hover:bg-red-700 text-xl' onClick={() => decrementCount(poster.id)}>-</button>
+          <div>
+            <ul className="text-white grid grid-cols-1 md:grid-cols-2 gap-2 text-xl">
+              {cartItems.map((poster) => (
+                <div className="w-full bg-gray-500 p-2 mt-2 rounded-md border-2 border-indigo-700">
+                  <li
+                    key={poster.id}
+                    className="flex items-center gap-1 md:gap-16"
+                  >
+                    <div>
+                      <h3>{poster.title}</h3>
+                      <img
+                        src={poster.imageUrl}
+                        alt={poster.title}
+                        className="rounded-lg border border-blue-500 border-opacity-50 h-[300px]"
+                      />
                     </div>
-                    <p>Total for this item: <span className='text-green-500 text-2xl'>${counts[poster.id] * poster.price}</span></p>
-                  </div>
-                </li>
-              </div>
-            ))}
-          </ul>
+                    <div>
+                      <p>Quantity: {counts[poster.id]}</p>
+                      <p>$$ per item: ${poster.price}</p>
+                      <div className="w-full">
+                        <button
+                          className="bg-green-500 p-2 rounded-md w-10 hover:bg-green-700 text-xl mt-2 mr-2"
+                          onClick={() => incrementCount(poster.id)}
+                        >
+                          +
+                        </button>
+                        <button
+                          className="bg-red-500 p-2 rounded-md w-10 hover:bg-red-700 text-xl"
+                          onClick={() => decrementCount(poster.id)}
+                        >
+                          -
+                        </button>
+                      </div>
+                      <p>
+                        Total for this item:{" "}
+                        <span className="text-green-500 text-2xl">
+                          ${(counts[poster.id] * poster.price).toFixed(2)}
+                        </span>
+                      </p>
+                    </div>
+                  </li>
+                </div>
+              ))}
+            </ul>
+            <div className="text-white text-2xl m-2">
+              Cart Total:{" "}
+              <span className="text-indigo-700 text-3xl">${cartTotal}</span>
+            </div>
+            <div>
+              <Link to="/payment">
+                <button className="bg-green-500 p-2 rounded-md w-full text-2xl hover:bg-green-700 mt-2">
+                  Proceed to Payment
+                </button>
+              </Link>
+            </div>
+          </div>
         ) : (
           <div>
-            <p className='text-white text-3xl mb-2'>Your cart is empty...</p>
-
+            <p className="text-white text-3xl mb-2">Your cart is empty...</p>
           </div>
         )}
       </div>
--- client/src/components/Payment.jsx
+++ client/src/components/Payment.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import cardValidator from "card-validator";
 import { Link } from "react-router-dom";
+import { RxArrowTopRight } from "react-icons/rx";
 
 const PaymentPage = () => {
   const [cardType, setCardType] = useState(""); // State to store the card type
@@ -56,130 +57,141 @@ const PaymentPage = () => {
   };
   return (
     <div className="flex items-center justify-center min-h-screen min-w-[350px] px-1">
-      <div className="w-[500px] mx-auto p-4 bg-white rounded-lg shadow-md">
-        <h2 className="text-2xl font-bold mb-4">Payment Information</h2>
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-          <div>
-            <label
-              htmlFor="firstname"
-              className="block text-sm font-medium text-gray-700"
-            >
-              First Name
-            </label>
-            <input
-              id="firstname"
-              {...register("firstName", { required: "First name is required" })}
-              placeholder="John"
-              className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-            />
-            {errors.firstName && (
-              <span className="text-red-500">{errors.firstName.message}</span>
+      <div>
+        <Link to="/cart" style={{ display: 'inline-block' }}>
+          <button className="flex group justify-center items-center gap-1 text-xl text-amber-500 bg-indigo-600 p-2 rounded-md hover:bg-indigo-700 mb-2 mr-auto">
+            BACK TO CART <RxArrowTopRight className="group-hover:rotate-45" />
+          </button>
+        </Link>
+        <div className="w-[500px] mx-auto p-4 bg-white rounded-lg shadow-md">
+          <h2 className="text-2xl font-bold mb-4">Payment Information</h2>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            <div>
+              <label
+                htmlFor="firstname"
+                className="block text-sm font-medium text-gray-700"
+              >
+                First Name
+              </label>
+              <input
+                id="firstname"
+                {...register("firstName", {
+                  required: "First name is required",
+                })}
+                placeholder="John"
+                className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+              />
+              {errors.firstName && (
+                <span className="text-red-500">{errors.firstName.message}</span>
+              )}
+
+              <label
+                htmlFor="lastname"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Last Name
+              </label>
+              <input
+                id="lastname"
+                {...register("lastName", { required: "Last name is required" })}
+                placeholder="Doe"
+                className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+              />
+              {errors.lastName && (
+                <span className="text-red-500">{errors.lastName.message}</span>
+              )}
+
+              <label
+                htmlFor="cardnumber"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Card Number
+              </label>
+              <input
+                id="cardnumber"
+                type="text"
+                placeholder="1234-5678-9012-3456"
+                {...register("cardNumber", {
+                  required: "Card number is required",
+                  validate: (value) => {
+                    const num = value.replace(/\D/g, "");
+                    const validation = cardValidator.number(num);
+                    setCardType(validation.card ? validation.card.type : "");
+                    return (
+                      (validation.isValid && num.length === 16) ||
+                      "This is not a valid card number. Card number must be 16 digits"
+                    );
+                  },
+                })}
+                onChange={formatCardNumber}
+                className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+              />
+              {errors.cardNumber && (
+                <span className="text-red-500">
+                  {errors.cardNumber.message}
+                </span>
+              )}
+
+              {/* removed &&cardType because didnt seem like we need it */}
+              {!errors.cardNumber && watchCardNumber && (
+                <span className="text-green-500 block">
+                  Card Type: {cardType.toUpperCase()}
+                </span>
+              )}
+
+              <label
+                htmlFor="cvv"
+                className="block text-sm font-medium text-gray-700"
+              >
+                CVV
+              </label>
+              <input
+                id="cvv"
+                type="text"
+                maxLength={3}
+                placeholder="123"
+                {...register("cvv", {
+                  required: "3 digit CVV is required - must be numbers only",
+                  maxLength: {
+                    value: 3,
+                    message: "CVV must be exactly 3 digits",
+                  },
+                })}
+                className="block w-1/6 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 text-center"
+                onChange={formatCVV}
+              />
+              {errors.cvv && (
+                <span className="text-red-500">{errors.cvv.message}</span>
+              )}
+            </div>
+
+            {isSubmitting && (
+              <p className="text-gray-500">Processing payment...</p>
+            )}
+            {showSuccessMessage && (
+              <p className="text-green-500">Payment successful!</p>
             )}
 
-            <label
-              htmlFor="lastname"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Last Name
-            </label>
-            <input
-              id="lastname"
-              {...register("lastName", { required: "Last name is required" })}
-              placeholder="Doe"
-              className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-            />
-            {errors.lastName && (
-              <span className="text-red-500">{errors.lastName.message}</span>
-            )}
-
-            <label
-              htmlFor="cardnumber"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Card Number
-            </label>
-            <input
-              id="cardnumber"
-              type="text"
-              placeholder="1234-5678-9012-3456"
-              {...register("cardNumber", {
-                required: "Card number is required",
-                validate: (value) => {
-                  const num = value.replace(/\D/g, "");
-                  const validation = cardValidator.number(num);
-                  setCardType(validation.card ? validation.card.type : "");
-                  return (
-                    (validation.isValid && num.length === 16) ||
-                    "This is not a valid card number. Card number must be 16 digits"
-                  );
-                },
-              })}
-              onChange={formatCardNumber}
-              className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-            />
-            {errors.cardNumber && (
-              <span className="text-red-500">{errors.cardNumber.message}</span>
-            )}
-
-            {/* removed &&cardType because didnt seem like we need it */}
-            {!errors.cardNumber && watchCardNumber && (
-              <span className="text-green-500 block">
-                Card Type: {cardType.toUpperCase()}
-              </span>
-            )}
-
-            <label
-              htmlFor="cvv"
-              className="block text-sm font-medium text-gray-700"
-            >
-              CVV
-            </label>
-            <input
-              id="cvv"
-              type="text"
-              maxLength={3}
-              placeholder="123"
-              {...register("cvv", {
-                required: "3 digit CVV is required - must be numbers only",
-                maxLength: {
-                  value: 3,
-                  message: "CVV must be exactly 3 digits",
-                },
-              })}
-              className="block w-1/6 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 text-center"
-              onChange={formatCVV}
-            />
-            {errors.cvv && (
-              <span className="text-red-500">{errors.cvv.message}</span>
-            )}
-          </div>
-
-          {isSubmitting && (
-            <p className="text-gray-500">Processing payment...</p>
-          )}
-          {showSuccessMessage && (
-            <p className="text-green-500">Payment successful!</p>
-          )}
-
-          <div className="w-full flex gap-2">
-            <button
-              type="submit"
-              disabled={isSubmitting}
-              className="w-full px-4 py-2 text-white bg-blue-500 rounded-md shadow-md hover:bg-blue-600"
-            >
-              Submit Payment
-            </button>
-            <Link to="/" className="w-full">
+            <div className="w-full flex gap-2">
               <button
                 type="submit"
                 disabled={isSubmitting}
-                className="w-full px-4 py-2 text-white bg-red-500 rounded-md shadow-md hover:bg-red-600"
+                className="w-full px-4 py-2 text-white bg-blue-500 rounded-md shadow-md hover:bg-blue-600"
               >
-                Cancel Payment
+                Submit Payment
               </button>
-            </Link>
-          </div>
-        </form>
+              <Link to="/" className="w-full">
+                <button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="w-full px-4 py-2 text-white bg-red-500 rounded-md shadow-md hover:bg-red-600"
+                >
+                  Cancel Payment
+                </button>
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
