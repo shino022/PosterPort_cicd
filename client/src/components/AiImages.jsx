d77c1b0337909842f2a3c4885e748353c4b5a39a
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-06-15 14:54:30 -0400

    Set up component to display queried image urls from db

--- client/src/components/AiImages.jsx
+++ client/src/components/AiImages.jsx
@@ -22,9 +22,9 @@ const AiImages = () => {
         <div>
             <h1>AI Images</h1>
             <div className="grid grid-cols-3 gap-4">
-                {images.map((image, index) => (
+                {images.length > 0 ? images.map((image, index) => (
                     <img key={index} src={image.image} alt="AI Generated" />
-                ))}
+                )): <div className='text-white'>No Ai generated images have been created yet...</div>}
             </div>
         </div>
     )
