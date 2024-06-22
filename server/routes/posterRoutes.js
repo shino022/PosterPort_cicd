aeb89c4365f1a89eba0002c55a7ea70f06df06df
shin <sshoony95@gmail.com>
2024-06-22 10:42:13 -0400

    trigger deploy

--- server/routes/posterRoutes.js
+++ server/routes/posterRoutes.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const posterController = require('../controllers/posterController');
+const posterController = require("../controllers/posterController");
 
 //Route to handle post form submission
 router.post("/submit", posterController.createPoster);
 
 //Route to get all posters
 router.get("/", posterController.getAllPosters);
-
 
 module.exports = router;
