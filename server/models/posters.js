5c72fc28a587a4b74d1ad0b757c9d25362d06176
shin <sshoony95@gmail.com>
2024-06-22 19:52:10 -0400

    Trigger deploy

--- server/models/posters.js
+++ server/models/posters.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose'); //always import in files concerning db
+const mongoose = require("mongoose"); //always import in files concerning db
 
 //create schema
 const posterSchema = new mongoose.Schema({
-    prompt: {
-        type: String,
-        required: true
-    },
-    image: {
-        type: String,
-        required: true
-    }
+  prompt: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
 });
 
-const Poster = mongoose.model('Poster', posterSchema);
+const Poster = mongoose.model("Poster", posterSchema);
 
 module.exports = Poster;
