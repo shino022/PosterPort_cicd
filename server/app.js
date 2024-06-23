49c95bb3396134d966c7aa35f10cd74a090aefd3
shin <sshoony95@gmail.com>
2024-06-22 20:08:01 -0400

    Delete mongodb

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const connectDB = require("./config/db");
 
 require("dotenv").config();
 
@@ -26,8 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-//initiate connection to server
-connectDB();
+
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
